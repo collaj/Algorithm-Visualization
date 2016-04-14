1cbfc3447469a0a5ab5071e555a2059cf3554453
James Colla <collajames@gmail.com>
2016-04-14 10:19:56 -0400

    Fixes a problem with TestAlgorithm.js

--- canvas/algorithm/TestAlgorithm.js
+++ canvas/algorithm/TestAlgorithm.js
@@ -1,6 +1,7 @@
 ﻿var TestAlgorithm = function () {
+	Algorithm.call(this);
+
 	this.name = "test";
-	this.objects = [];
 };
 
 extend(Algorithm, TestAlgorithm);
@@ -8,6 +9,8 @@ extend(Algorithm, TestAlgorithm);
 
 
 TestAlgorithm.prototype.draw = function (canvas) {
+	Algorithm.prototype.draw.call(this, canvas);
+
 	var thisLoop = new Date;
 	var fps = 1000 / (thisLoop - this.lastLoop);
 	this.lastLoop = thisLoop;
@@ -24,60 +27,56 @@ TestAlgorithm.prototype.draw = function (canvas) {
 
 TestAlgorithm.prototype.load = function () {
 
-	for (var i = 0; i < 10; i++) {
+	for (var i = 0; i < 50; i++) {
 		var coor = new Coordinate2D(Math.random() * Engine.canvas.width, Math.random() * Engine.canvas.height);
 		var circle = new Circle(coor, (Math.random() * 10) + 5);
 
-		this.addObject(circle);
+		this.addObjects(circle);
 	}
 
 	for (var j = 0; j < this.objects.length; j++) {
 
-	    var startTime = 0;
+		var startTime = 0;
 
-		var animationLength = 10000;
+		var animationLength = 1000;
 
 		//var hit = false;
 
-		for (var i = 0; i < 100; i++) {
+		for (var i = 0; i < 50; i++) {
 			
 			var col = new Color();
 			col.becomeRandomColor();
 
 			var colorAnimation = new ColorAnimation(startTime, animationLength, this.objects[j], col);
-			this.objects[j].animations.addAnimation(colorAnimation);
+			this.animations.addAnimation(colorAnimation);
 
 
 			var coor = new Coordinate2D((Math.random() * Engine.canvas.width) - 50, (Math.random() * Engine.canvas.height) - 50);
 
 
 			var moveAnimation = new MoveAnimation(startTime, animationLength, this.objects[j], coor);
-			this.objects[j].animations.addAnimation(moveAnimation);
+			this.animations.addAnimation(moveAnimation);
 
 
 			startTime = startTime + animationLength;
 
-			/*coor = new Coordinate2D(Math.random() * Engine.canvas.width +100, Math.random() * Engine.canvas.height  +100);
+			/*
+			coor = new Coordinate2D(Math.random() * Engine.canvas.width +100, Math.random() * Engine.canvas.height  +100);
 				
 			var newAnimationLength = (Math.random() * 100) + 400;
 			moveAnimation = new MoveAnimation(startTime, newAnimationLength, this.objects[j], coor);
-			this.objects[j].animations.addAnimation(moveAnimation);
-
+			this.animations.addAnimation(moveAnimation);
 
 			startTime = startTime + newAnimationLength;
 			*/
+			
 			
 		}
 
 	}
 
+	Engine.start(10);
 
 
-
-};
-
-
-TestAlgorithm.prototype.addObject = function (element) {
-	this.objects.push(element);
 };
 
